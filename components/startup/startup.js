27bca0541d012099b8688c87a5795707f8816bea
Protik Sarkar <protik.sarkar@gmail.com>
2020-07-13 22:51:14 +0200

    added starting loading

--- components/startup/startup.js
+++ components/startup/startup.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactLogo from '../react-logo/react-logo';
 import './startup.scss';
 
 const Startup = () => {
   return (
     <div className="startup">
-      <ReactLogo />
+      <iframe src="https://giphy.com/embed/xTkcEQACH24SMPxIQg" width="250" height="250" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/hand-bored-waiting-xTkcEQACH24SMPxIQg"></a></p>
     </div>
   );
 };
